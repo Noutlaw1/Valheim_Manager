918467fb229e19956919ac10fdae4f43c0dbbfe1
Nick Outlaw <nickoutlaw@gmail.com>
2021-02-27 15:48:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 class Config(object):
         #Add an array of IPs here that so they are allowed access.
         ALLOWED_IPS = ['']
+        #Path to wherever the Valheim docker container is stored, if you want to run a backup script there.
+        VALHEIM_DIR = ""
+        #Valheim backup script name.
+        BACKUP_SCRIPT = ""
